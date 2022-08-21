a45fa50419aa3a539d5140bcbedaa7483f5a2d35
ricklegac <rick.legac@gmail.com>
2022-08-21 19:21:40 -0400

    no murio?

--- trello-poli/trello-poli/trello-poli/wsgi.py
+++ trello-poli/trello-poli/trello-poli/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sso project.
+WSGI config for trello-poli project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sso.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trello-poli.settings')
 
 application = get_wsgi_application()
