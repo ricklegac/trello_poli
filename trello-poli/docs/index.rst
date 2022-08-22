59339f80ca81fd8ecb2d24f90bccc2b137bf5d11
ricklegac <rick.legac@gmail.com>
2022-08-21 23:10:33 -0400

    ge

---
+++ trello-poli/docs/index.rst
@@ -0,0 +1,8 @@
+Welcome to Trello Poli's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   trello-poli/docs/doc
--- trello-poli/requerimientos.sh
+++ trello-poli/requerimientos.sh
@@ -1,4 +1,8 @@
 python3 -m pip install Django
 pip install django-allauth #allaut de django
 python3 -m pip install psycopg2 #para enlazar con postgresql
+pip install django-crispy-forms #form para djangopython3 -m pip install Django
+pip install django-allauth #allaut de django
+python3 -m pip install psycopg2 #para enlazar con postgresql
 pip install django-crispy-forms #form para django
+pip3 install -U Sphinx
