bda58cdc37b8166cb8e75bc6c50f36e16b6579a9
ricklegac <rick.legac@gmail.com>
2022-08-21 21:25:13 -0400

    req

---
+++ requerimientos.sh
@@ -0,0 +1,4 @@
+python3 -m pip install Django
+pip install django-allauth #allaut de django
+python3 -m pip install psycopg2 #para enlazar con postgresql
+pip install django-crispy-forms #form para django
