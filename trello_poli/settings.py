dafe39c32976a548c1eb16e7770dc8548dcdade4
ricklegac <rick.legac@gmail.com>
2022-08-27 22:58:00 -0400

    sso sin template

--- trello_poli/settings.py
+++ trello_poli/settings.py
@@ -67,11 +67,20 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request'
             ],
         },
     },
 ]
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
 
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
 WSGI_APPLICATION = 'trello_poli.wsgi.application'
 
 
