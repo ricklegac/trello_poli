f67a3acdaeec2eac5d2d3d451c017fa30dcbaa48
lorencab <lorenzo@girolabs.com>
2022-09-30 15:41:52 -0400

    Register all endpoints

--- is2_sgpa/sgpa/apps/proyectos/admin.py
+++ is2_sgpa/sgpa/apps/proyectos/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from proyectos.models import Proyecto, Sprint
+from proyectos.models import Miembro, Proyecto, Rol, Sprint, UserStory
 
 # Register your models here.
 admin.site.register(Proyecto)
 admin.site.register(Sprint)
+admin.site.register(Miembro)
+admin.site.register(Rol)
+admin.site.register(UserStory)
--- is2_sgpa/sgpa/apps/roles/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from roles.models import Rol
-
-# Register your models here.
-admin.site.register(Rol)
